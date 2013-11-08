91ed0f7a0e08baf5108c06f9f76db8dcec55625f
Andy Hansen <ahansen.it@gmail.com>
2013-11-08 19:30:50 +1300

    It will now send your results in the facebook message

--- ResultsChecker.py
+++ ResultsChecker.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import getpass
 
 username = raw_input("Enter your uni username: ")
-password = raw_input("Enter your uni password: ")
+password = getpass.getpass("Enter your uni password: ")
 fbusername = raw_input("Enter your facebook email: ")
-fbpassword = raw_input("Enter your facebook password: ")
+fbpassword = getpass.getpass("Enter your facebook password: ")
 fbmessage = raw_input("Enter the url of your facebook inbox: ")
 
 driver = webdriver.Firefox()
+#load the pims page
 driver.get("https://corpapp.otago.ac.nz/pims")
 sleep(5) #wait for redirects
 elem = driver.find_element_by_name("username")
@@ -17,16 +19,20 @@ elem = driver.find_element_by_name("password")
 elem.send_keys(password)
 elem.send_keys(Keys.RETURN)
 
+driver.get("https://corpapp.otago.ac.nz/apex/f?p=128:91")
+old_text = driver.find_elements_by_class_name("t103RegionWithoutButtonsAndTitle")[1].text
 #keep checking the results page until Unconfirmed comes up
 changed = False
 while not changed:
-  sleep(120)
-  print "Checking"
   driver.get("https://corpapp.otago.ac.nz/apex/f?p=128:91")
-  #When unconfirmed has changed exit the loop
-  if "Unconfirmed" in driver.page_source:
+  new_text = driver.find_elements_by_class_name("t103RegionWithoutButtonsAndTitle")[1].text
+  if old_text != new_text:
     changed = True
+  old_text = new_text
+  sleep(10)
 
+print "The results are up"
+#The code that seems a message to you on facebook
 driver.get("https://www.facebook.com")
 elem = driver.find_element_by_name("email")
 elem.send_keys(fbusername)
@@ -36,7 +42,7 @@ elem.send_keys(Keys.RETURN)
 #change this to your facebook inbox
 driver.get(fbmessage)
 elem = driver.find_element_by_name("message_body")
-elem.send_keys("Results are up")
+elem.send_keys(old_text)
 elem.send_keys(Keys.RETURN)
 
 driver.close()
