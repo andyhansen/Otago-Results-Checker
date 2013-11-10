b3e5e22745878b7b9ae2d3c91647164272e12396
Andy Hansen <ahansen.it@gmail.com>
2013-11-10 17:01:05 +1300

    Make the code a bit cleaner and added some comments

--- ResultsChecker.py
+++ ResultsChecker.py
@@ -2,15 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import getpass
+from time import strftime, localtime
 
-username = raw_input("Enter your uni username: ")
-password = getpass.getpass("Enter your uni password: ")
+"""
+Sends the provided message to the given facebook inbox. The
+message will be sent from the account whos login details are provided.
+"""
+def sendFacebookMessage(username, password, inboxUrl, message):
+  driver.get("https://www.facebook.com")
+  elem = driver.find_element_by_name("email")
+  elem.send_keys(username)
+  elem = driver.find_element_by_name("pass")
+  elem.send_keys(password)
+  elem.send_keys(Keys.RETURN)
+  driver.get(inboxUrl)
+  elem = driver.find_element_by_name("message_body")
+  elem.send_keys(message)
+  elem.send_keys(Keys.RETURN)
+
+username = raw_input("Enter your pims username: ")
+password = getpass.getpass("Enter your pims password: ")
 fbusername = raw_input("Enter your facebook email: ")
 fbpassword = getpass.getpass("Enter your facebook password: ")
-fbmessage = raw_input("Enter the url of your facebook inbox: ")
+fbmessage = raw_input("Enter the url for your facebook inbox: ")
 
 driver = webdriver.Firefox()
-#load the pims page
+print "This is a basic program which checks your pims every two minutes."
+print "If your results have been updated then it sends the changed results"
+print "as a Facebook message to the Facebook inbox you choose."
+print
+
+#log into pims
 driver.get("https://corpapp.otago.ac.nz/pims")
 sleep(5) #wait for redirects
 elem = driver.find_element_by_name("username")
@@ -19,33 +41,26 @@ elem = driver.find_element_by_name("password")
 elem.send_keys(password)
 elem.send_keys(Keys.RETURN)
 
+#takes you to your results page on pims then takes the text inside the
+#results box and prints it to the terminal
 driver.get("https://corpapp.otago.ac.nz/apex/f?p=128:91")
 old_text = driver.find_elements_by_class_name("t103RegionWithoutButtonsAndTitle")[1].text
-#keep checking the results page until Unconfirmed comes up
-changed = False
-while not changed:
+print old_text + "\n"
+
+"""
+Reloads the page and checks the results every two minutes.
+If the results are up then it sends a message to the facebook
+inbox specified from the account whos log in details are provided.
+"""
+while True:
   driver.get("https://corpapp.otago.ac.nz/apex/f?p=128:91")
   new_text = driver.find_elements_by_class_name("t103RegionWithoutButtonsAndTitle")[1].text
+  # If the results have changed then send the facebook message
   if old_text != new_text:
-    changed = True
+    sendFacebookMessage(fbusername, fbpassword, fbmessage, old_text)
+    break
   old_text = new_text
-  #the line below is just to give the user some feedback, comment it
-  #out if you don't care
-  print old_text, "\n"
+  print "Last checked at:", strftime("%d/%m/%Y %H:%M", localtime())
   sleep(120)
-
-print "The results are up"
-#The code that seems a message to you on facebook
-driver.get("https://www.facebook.com")
-elem = driver.find_element_by_name("email")
-elem.send_keys(fbusername)
-elem = driver.find_element_by_name("pass")
-elem.send_keys(fbpassword)
-elem.send_keys(Keys.RETURN)
-#change this to your facebook inbox
-driver.get(fbmessage)
-elem = driver.find_element_by_name("message_body")
-elem.send_keys(old_text)
-elem.send_keys(Keys.RETURN)
 
 driver.close()
